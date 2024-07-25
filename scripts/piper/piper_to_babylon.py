6741b2c9c1f8b1014665caf07739f576ddae54b2
danemadsen <dane_madsen@hotmail.com>
2024-07-25 14:35:38 +1000

    phonemes meta

--- scripts/piper/piper_to_babylon.py
+++ scripts/piper/piper_to_babylon.py
@@ -1,13 +1,30 @@
+import json
 import onnx
 
 onnx_file_path = './en_US-curie-medium-V1.4.onnx'
+config_file_path = './en_US-curie-medium-V1.4.onnx.json'
 
 # Verify the ONNX model
 onnx_model = onnx.load(onnx_file_path)
 
+# Load JSON data from file
+with open(config_file_path, 'r') as file:
+    json_data = file.read()
+
+# Load JSON data
+data = json.loads(json_data)
+
+# Extract phoneme_id_map
+phoneme_id_map = data['phoneme_id_map']
+
+# Create strings for phonemes and phoneme IDs
+phonemes = ' '.join(phoneme_id_map.keys())
+phoneme_ids = ' '.join(str(num[0]) for num in phoneme_id_map.values())
+
 # Add metadata to the ONNX model
 metadata = {
-    "language": "en_us",
+    "phonemes": phonemes,
+    "phoneme_ids": phoneme_ids
 }
 
 for key, value in metadata.items():
