41146289b73420508cfa607a9358ad1dd2e4ba1d
danemadsen <dane_madsen@hotmail.com>
2024-07-31 09:51:42 +1000

    example

--- example/main.c
+++ example/main.c
@@ -3,7 +3,7 @@
 int main() {
     babylon_g2p_init("./models/deep_phonemizer.onnx", "en_us", 1);
 
-    const char* text = "Hello world. There is 317 characters in this sentence.";
+    const char* text = "Hello world. There is over 300 characters in this sentence. This is an example program for the Babylon project. Text to speech models can be used to generate speech from text. This is a very powerful tool for many applications. For example, it can be used to generate speech for virtual assistants, audiobooks, and more.";
 
     babylon_tts_init("./models/curie.onnx");
 
